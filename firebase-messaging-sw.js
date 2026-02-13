fd520748abbacb2674d60b124d93199dd2ed10c3
uangkas <uangkasdriverhelper@gmail.com>
2026-02-13 21:32:18 +0700

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "ISI_APIKEY",
+  apiKey: "AIzaSyAWuaS0RpcxZ_gU0B_4DMmqetkYnKuvlYM",
   authDomain: "webkas-843cb.firebaseapp.com",
   projectId: "webkas-843cb",
   messagingSenderId: "290112385940",
-  appId: "ISI_APP_ID"
+  appId: ":290112385940:web:bf36abc7542b693334d581"
 });
 
 const messaging = firebase.messaging();
